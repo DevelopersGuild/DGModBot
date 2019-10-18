b6d4e6f413610d1cfc7a674ae45c3603f4387ace
Drabblesaur <tojohnny1559@gmail.com>
2019-10-18 14:14:41 -0700

    Yeet

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 
 const RoboCommands = (msg) => ({
     "website": () => msg.reply('https://www.da-developers.dev'),
-    "bot info": () => msg.reply('Version 1.0.1'), 
+    "bot info": () => msg.reply('Version 1.0.1'),
     "hits vape": () => msg.reply('🥬💨'),
     "YEET": () => msg.reply('YOTE'),
     "ping": () => msg.reply('🏓pong')
 });
 
-const LeadCommands = (msg) => ({
+const TonyCommands = (msg) => ({
     "lead": () => msg.reply('👌 You are a Leadership Member')
 });
 
@@ -20,22 +20,29 @@ const LeadCommands = (msg) => ({
  */
 const handleMessage = (msg) => {
     const PREFIX = 'DG!';
+    const TonyKey = 'Tony=>'
     const msgs = msg.content.split(" ")
     const roboCommands = RoboCommands(msg);
-    let role = msg.member.guild.roles.find('name','Leadership')
+    let role = msg.member.guild.roles.find('name', 'Leadership')
     if (msgs[0] == PREFIX) {
         // getting rid of DG! prefix
         msgs.splice(0, 1);
+        if (msgs[0] == TonyKey) {
+            if (msgs.length === 0 || msgs == undefined) {
+                msg.reply('At least one argument is needed to complete a task.');
+            } else {
+                if (TonyCommands[msgs.join(" ")]) {
+                    TonyCommands[msgs.join(" ")]();
+                } else {
+                    msg.reply('The command you entered is could not be found.')
+                }
+            }
+        }
         if (msgs.length === 0 || msgs == undefined) {
             msg.reply('At least one argument is needed to complete a task.');
         } else {
-            if (roboCommands[msgs.join(" ")] || LeadCommands[msgs.join(" ")] ) {
-                if(role.id === msg.member.guild.roles.find('name','Leadership').id){
-                    roboCommands[msgs.join(" ")]();
-                    LeadCommands[msgs.join(" ")];
-                }else{
-                    roboCommands[msgs.join(" ")]();
-                }
+            if (roboCommands[msgs.join(" ")]) {
+                roboCommands[msgs.join(" ")]();
             } else {
                 msg.reply('The command you entered is could not be found.')
             }
@@ -55,13 +62,13 @@ bot.on('guildMemberAdd', member => {
     channel.send(`Welcome to the server, ${member} be sure to read #rules 👋`);
 });
 
-bot.on('message', message =>{
-    if(message.channel.name == 'rules'){
-    let role = message.member.guild.roles.find('name','Unapproved');
-    message.member.removeRole(role);
-    message.delete(0)
-    .then(msg => console.log(`Deleted message from ${msg.author.username}`))
-    .catch(console.error);
+bot.on('message', message => {
+    if (message.channel.name == 'rules') {
+        let role = message.member.guild.roles.find('name', 'Unapproved');
+        message.member.removeRole(role);
+        message.delete(0)
+            .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+            .catch(console.error);
     }
 });
 
